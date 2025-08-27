19da56874f0bca67ff5b4c955c681c5f48b2b8d0
Arnold Paye <arnoldpayet@gmail.com>
2025-08-27 16:48:58 -0400

    feat: more flexbox

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "stretch",
+      }}
+    >
       <View
         style={{
           backgroundColor: "red",
-          width: 100,
-          height: 100,
+          flex: 1,
           justifyContent: "center",
           alignItems: "center",
         }}
@@ -17,8 +25,7 @@ export default function App() {
       <View
         style={{
           backgroundColor: "blue",
-          width: 100,
-          height: 100,
+          flex: 2,
           justifyContent: "center",
           alignItems: "center",
         }}
@@ -28,8 +35,6 @@ export default function App() {
       <View
         style={{
           backgroundColor: "green",
-          width: 100,
-          height: 100,
           justifyContent: "center",
           alignItems: "center",
         }}
